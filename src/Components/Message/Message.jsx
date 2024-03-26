86cbd458a809c97d6fdcaa6f95253b78d1888d3f
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-27 00:00:55 +0600

    added new return

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -1,4 +1,16 @@
 const Message = ({ user, message, classs }) => {
+  if (user) {
+    return (
+      <div>
+        <div className="mt-5 w-[40%]">
+          <h1 className="bg-gradient-to-t from-indigo-500 via-purple-500 to-pink-500 py-5 px-5 rounded-3xl text-white">
+            Messages here {message}
+          </h1>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="mt-5 w-[40%]">
