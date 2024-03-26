cac847fdbf6116081698bad30ef0bee62c0db759
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-26 17:35:51 +0600

    added socket io client

--- src/Components/Interface/Interface.jsx
+++ src/Components/Interface/Interface.jsx
@@ -1,13 +1,15 @@
-import socketIO from 'socketIO';
+import socketIO from 'socket.io-client';
 
 const ENDPOINT = 'http://localhost:5000/';
 
-const socket = socketIO(ENDPOINT, {transports: ['websocket']});
+const socket = socketIO(ENDPOINT, { transports: ['websocket'] });
 
 const Interface = () => {
+  socket.on('connect', () => {});
+
   return (
     <div>
-      
+      <h1>HAha</h1>
     </div>
   );
 };
